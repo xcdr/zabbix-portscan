4ca5f016f3749a01b07a823409225bba19dfdc88
Adam Kubica <xcdr@kaizen-step.com>
2024-01-17 22:34:57 +0100

    Better exception handling, flake8 fixes

--- zabbix_portscan/scanner.py
+++ zabbix_portscan/scanner.py
@@ -8,14 +8,16 @@ import ipaddress
 from zappix.sender import Sender
 
 
+# Split network to list of hosts
 def addressess(network: str) -> list[str]:
     return [str(ip) for ip in ipaddress.ip_network(network, strict=False)]
 
 
-def scan(ip: str, range: str = '1-65535', args='') -> dict[str, list]:
+# Scan network for specified port range
+def scan(network: str, range: str = '1-65535', args='') -> dict[str, list]:
     result = {}
     scanner = nmap.PortScanner()
-    scanner.scan(ip, range, arguments=args)
+    scanner.scan(network, range, arguments=args)
 
     for host in scanner.all_hosts():
         if scanner[host].state() == 'up':
@@ -25,6 +27,7 @@ def scan(ip: str, range: str = '1-65535', args='') -> dict[str, list]:
     return result
 
 
+# Send metrics to zabbix server
 def send_metrics(zabbix_ip: str, zabbix_host: str, scan_result: dict[str, list]):
     sender = Sender(zabbix_ip)
 
@@ -57,14 +60,20 @@ def send_metrics(zabbix_ip: str, zabbix_host: str, scan_result: dict[str, list])
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Zabbix NMAP open port monitor, author Adam Kubica <xcdr@kaizen-step.com>')
+    parser = argparse.ArgumentParser(description='Zabbix open port monitor, author Adam Kubica <xcdr@kaizen-step.com>')
 
-    parser.add_argument('--host', required=True, help='host the data belongs to. (this host should have the NMAP template)')
-    parser.add_argument('--server', required=True, help='zabbix server (default: 127.0.0.1)', default='127.0.0.1')
-    parser.add_argument('--network', required=True, help='network to scan')
-    parser.add_argument('--ports', required=False, help='ports to scan (default: 1-1024)', default='1-1024')
-    parser.add_argument('--verbose', required=False, default=False, help='log info to stdout', action=argparse.BooleanOptionalAction)
-    parser.add_argument('nmap_params', nargs='*', help='additional NMAP parameters')
+    parser.add_argument('--host', required=True,
+                        help='host the data belongs to. (this host should have the NMAP template)')
+    parser.add_argument('--server', required=True,
+                        help='zabbix server (default: 127.0.0.1)', default='127.0.0.1')
+    parser.add_argument('--network', required=True,
+                        help='network to scan')
+    parser.add_argument('--ports', required=False,
+                        help='ports to scan (default: 1-1024)', default='1-1024')
+    parser.add_argument('--verbose', required=False, default=False, action=argparse.BooleanOptionalAction,
+                        help='log info to stdout')
+    parser.add_argument('nmap_params', nargs='*',
+                        help='additional NMAP parameters')
     args = parser.parse_args()
 
     if args.verbose:
@@ -79,10 +88,10 @@ if __name__ == '__main__':
         else:
             logging.info(f"No open ports for: {args.network}")
     except nmap.PortScannerError as e:
-        logging.exception(e)
+        logging.error(e)
         exit(1)
     except ConnectionRefusedError as e:
-        logging.exception(e)
+        logging.error(e)
         exit(1)
     except KeyboardInterrupt:
         exit(0)
